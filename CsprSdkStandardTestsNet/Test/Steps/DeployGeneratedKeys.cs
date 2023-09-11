077979150c4d49a8a1ebfc900b25b69ae1a893c0
stormeye2000 <info@stormeye.co.uk>
2023-09-11 11:33:29 +0100

    all steps coded

--- CsprSdkStandardTestsNet/Test/Steps/DeployGeneratedKeys.cs
+++ CsprSdkStandardTestsNet/Test/Steps/DeployGeneratedKeys.cs
@@ -1,5 +1,11 @@
-using System.IO;
+using System;
+using System.Numerics;
+using System.Threading.Tasks;
+using Casper.Network.SDK;
+using Casper.Network.SDK.SSE;
 using Casper.Network.SDK.Types;
+using CsprSdkStandardTestsNet.Test.Utils;
+using NUnit.Framework;
 using TechTalk.SpecFlow;
 using static System.Console;
 
@@ -8,55 +14,106 @@ namespace CsprSdkStandardTestsNet.Test.Steps;
 [Binding]
 public class DeployGeneratedKeys {
     
+    private readonly ContextMap _contextMap = ContextMap.Instance;    
+    private static NetCasperClient GetCasperService() {
+        return CasperClientProvider.GetInstance().CasperService;
+    }
+    
     [Given(@"that a ""(.*)"" sender key is generated")]
     public void GivenThatASenderKeyIsGenerated(string algo) {
         WriteLine("that a {0} sender key is generated", algo);
 
-        // var keyPair = KeyPair.CreateNew(KeyAlgo.ED25519);
-        // var pk = keyPair.PublicKey;
+        var keyPair = KeyPair.CreateNew(algo.Equals("Ed25519") ? KeyAlgo.ED25519 : KeyAlgo.SECP256K1);
 
-        // var gen = new Ed25519KeyPairGenerator();
-        // gen.Init(new KeyGenerationParameters(new SecureRandom(), 255));
-        // var newKey = gen.GenerateKeyPair();
-        // var publicKey = (Ed25519PublicKeyParameters)newKey.Public;
-        //
-        // var pk = PublicKey.FromRawBytes(publicKey.GetEncoded(), KeyAlgo.ED25519);
-        // var sk = newKey.Private;
-
-
-        var keyPair = KeyPair.CreateNew(KeyAlgo.ED25519);
-        var tmpfile = Path.GetTempFileName();
-        File.Delete(tmpfile);
-            
-        keyPair.WriteToPem(tmpfile);
-
-        var keyPair2 = KeyPair.FromPem(tmpfile);
+        Assert.That(keyPair, Is.Not.Null);
+        Assert.That(keyPair.PublicKey, Is.Not.Null);
         
+        _contextMap.Add("SENDER_KEY_PAIR", keyPair);
 
     }
 
     [Then(@"fund the account from the faucet user with a transfer amount of (.*) and a payment amount of (.*)")]
-    public void ThenFundTheAccountFromTheFaucetUserWithATransferAmountOfAndAPaymentAmountOf(long transferAmount, long paymentAmount) {
+    public async Task ThenFundTheAccountFromTheFaucetUserWithATransferAmountOfAndAPaymentAmountOf(string transferAmount, string paymentAmount) {
         WriteLine("fund the account from the faucet user with a transfer amount of {0} and a payment amount of {1}", transferAmount, paymentAmount);
+        
+        var faucetPem = AssetUtils.GetFaucetAsset(1, "secret_key.pem");
+        Assert.That(faucetPem, Is.Not.Null);
+        
+        var faucetKey = KeyPair.FromPem(faucetPem);
+
+        Assert.That(faucetKey, Is.Not.Null);
+        Assert.That(faucetKey.PublicKey, Is.Not.Null);
+
+        _contextMap.Add(StepConstants.TRANSFER_AMOUNT, BigInteger.Parse(transferAmount));
+        _contextMap.Add(StepConstants.PAYMENT_AMOUNT, BigInteger.Parse(paymentAmount));
+
+        await DoDeploy(faucetKey, _contextMap.Get<KeyPair>("SENDER_KEY_PAIR"));
+        
         
     }
 
     [Given(@"that a ""(.*)"" receiver key is generated")]
     public void GivenThatAReceiverKeyIsGenerated(string algo) {
         WriteLine("that a {0} receiver key is generated", algo);
+
+        var keyPair = KeyPair.CreateNew(algo.Equals("Ed25519") ? KeyAlgo.ED25519 : KeyAlgo.SECP256K1);
+
+        Assert.That(keyPair, Is.Not.Null);
+        Assert.That(keyPair.PublicKey, Is.Not.Null);
+
+        byte[] msg = "this is the receiver"u8.ToArray();
+        byte[] signature = keyPair.Sign(msg);
         
+        Assert.That(keyPair.PublicKey.VerifySignature(msg, signature), Is.True);
+        
+        _contextMap.Add("RECEIVER_KEY_PAIR", keyPair);
+
     }
 
     [Then(@"transfer to the receiver account the transfer amount of (.*) and the payment amount of (.*)")]
-    public void ThenTransferToTheReceiverAccountTheTransferAmountOfAndThePaymentAmountOf(long transferAmount, long paymentAmount) {
+    public async Task ThenTransferToTheReceiverAccountTheTransferAmountOfAndThePaymentAmountOf(string transferAmount, string paymentAmount) {
         WriteLine("transfer to the receiver account the transfer amount of {0} and the payment amount of {1}", transferAmount, paymentAmount);
+        
+        _contextMap.Add(StepConstants.TRANSFER_AMOUNT, BigInteger.Parse(transferAmount));
+        _contextMap.Add(StepConstants.PAYMENT_AMOUNT, BigInteger.Parse(paymentAmount));
+
+        await DoDeploy(_contextMap.Get<KeyPair>("SENDER_KEY_PAIR"), _contextMap.Get<KeyPair>("RECEIVER_KEY_PAIR"));
         
     }
     
     
     [Then(@"the transfer approvals signer contains the ""(.*)"" algo")]
-    public void ThenTheTransferApprovalsSignerContainsTheAlgo(string algo) {
+    public async Task ThenTheTransferApprovalsSignerContainsTheAlgo(string algo) {
         WriteLine("the transfer approvals signer contains the {0} algo", algo);
         
+        var matchingBlockHash =  _contextMap.Get<BlockAdded>(StepConstants.LAST_BLOCK_ADDED);
+        
+        var block = await GetCasperService().GetBlock(matchingBlockHash.BlockHash);
+
+        Assert.That(block, Is.Not.Null);
+        Assert.That(block.Parse().Block.Body.Proposer.KeyAlgorithm.ToString(), Is.EqualTo(algo.ToUpper()));
+
     }
+    
+    private async Task DoDeploy(KeyPair senderKeys, KeyPair receiverKeys) {
+        
+        Deploy deploy = DeployTemplates.StandardTransfer(
+            senderKeys.PublicKey,
+            receiverKeys.PublicKey,
+            _contextMap.Get<BigInteger>(StepConstants.TRANSFER_AMOUNT),
+            _contextMap.Get<BigInteger>(StepConstants.PAYMENT_AMOUNT),
+            "casper-net-1",
+            null,
+            1,
+            (ulong)TimeSpan.FromMinutes(30).TotalMilliseconds);
+        
+        deploy.Sign(senderKeys);
+        
+        var putResponse = await GetCasperService().PutDeploy(deploy);
+        
+        _contextMap.Add(StepConstants.DEPLOY_RESULT, putResponse);
+
+    }
+    
+    
 }
